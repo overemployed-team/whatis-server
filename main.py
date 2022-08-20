bcfd8dcbf4aeea450d3081bbb09a813ad7df8b59
alex <format37@gmail.com>
2022-08-20 15:49:07 +0300

    modified:   main.py 	modified:   run_local.sh 	new file:   run_server.sh

--- main.py
+++ main.py
@@ -5,19 +5,42 @@ import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
+API_KEY = os.environ.get('API_KEY','')
+co = cohere.Client(API_KEY)
+
+
+def what_prompt(user_prompt):
+    # read init phrase, readilng from what_init.txt
+    with open('what_init.txt', 'r') as f:
+        init_phrase = str(f.read())
+    return init_phrase+user_prompt['question']+'\nWhat is it?\nA:'
+
+
+def generate_answer(prompt):
+    generation = co.generate(
+        model='xlarge',
+        prompt= prompt,
+        max_tokens=200,
+        temperature=1,
+        stop_sequences=["--"],
+        k=0,
+        p=0.75
+        )
+    return generation.generations[0].text.replace('\n--','').strip()
+
 
 @app.route('/what', methods=['POST'])
 def what_is():
     content = request.json
     print(content)
     # TODO[]: get the question and tell the answer to response
-
-    fake_response = [
+    prompt = what_prompt(content)
+    response = [
         {
-            "answer": "well, we didn't implement that yet."
+            "answer": generate_answer(prompt)
         }
     ]
-    return jsonify(fake_response)
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(
--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,5 @@
 # set PORT environment = 8087
-export PORT=8087
-export HOST=127.0.0.1
+export PORT=8080
+export HOST=api.whatisit.app
 export API_KEY=LEHy30tA8Lb3IiszzMgi2cAHvUdygD59LAAy7O3W
 python main.py
