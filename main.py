fe42a29ae6706514ddbbba9b4cf2b86936034794
Supawat Tamsri <supawat@intelek.ai>
2022-08-20 10:59:03 +0200

    updated for dockerization

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify  
 from flask_cors import CORS
 import cohere
-
+import os
 def create_app(test_config=None):
     print("Server started")
     app = Flask(__name__)
@@ -21,4 +21,8 @@ def create_app(test_config=None):
         return jsonify(fake_response)
 
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
