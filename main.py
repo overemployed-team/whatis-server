fdf4d57ba0033b025dd30d85564a229521a026d3
Supawat Tamsri <supawat@intelek.ai>
2022-08-20 11:13:20 +0200

    restructure for gunicorn

--- main.py
+++ main.py
@@ -2,27 +2,23 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import cohere
 import os
-def create_app(test_config=None):
-    print("Server started")
-    app = Flask(__name__)
-    CORS(app)
 
-    @app.route('/what', methods=['POST'])
-    def what_is():
-        content = request.json
-        print(content)
-        # TODO[]: get the question and tell the answer to response
+app = Flask(__name__)
+CORS(app)
 
-        fake_response = [
-            {
-                "answer": "well, we didn't implement that yet."
-            }
-        ]
-        return jsonify(fake_response)
+@app.route('/what', methods=['POST'])
+def what_is():
+    content = request.json
+    print(content)
+    # TODO[]: get the question and tell the answer to response
 
-    return app
+    fake_response = [
+        {
+            "answer": "well, we didn't implement that yet."
+        }
+    ]
+    return jsonify(fake_response)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
